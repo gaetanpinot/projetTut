6359fe4d4b2a3123de893a21d4356ac61b3f4352
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-12-17 15:25:42 +0100

    base sql terminé

--- sql/amap.sql
+++ sql/amap.sql
@@ -1,3 +1,27 @@
+drop table if exists
+ allergene,
+ allergene_ingredient,
+ allergie_utilisateur,
+ commentaire,
+ favoris,
+ frigo,
+ ingredient,
+ ingredient_exclu,
+ ingredient_panier,
+ ingredient_produit,
+ ingredient_recette,
+ note_recette,
+ panier,
+ panier_utilisateur,
+ producteur_utilisateur,
+ recette,
+ tag,
+ tag_recette,
+ ustensile,
+ ustensile_exclu,
+ ustensile_recette,
+ utilisateur CASCADE;
+
 CREATE TABLE "utilisateur" (
   "id" uuid PRIMARY KEY,
   "nom_utilisateur" varchar UNIQUE,
@@ -30,6 +54,12 @@ CREATE TABLE "ingredient_panier" (
   PRIMARY KEY ("id_panier", "id_ingredient")
 );
 
+CREATE TABLE "panier_utilisateur" (
+  "id_utilisateur" uuid,
+  "id_panier" int,
+  PRIMARY KEY ("id_utilisateur", "id_panier")
+);
+
 CREATE TABLE "recette" (
   "id" serial PRIMARY KEY,
   "nom" varchar,
@@ -37,10 +67,16 @@ CREATE TABLE "recette" (
   "complexite" int,
   "id_createur" uuid,
   "description" text,
-  "debut_saison" date,
-  "fin_saison" date,
+  "debut_saison" int,
+  "fin_saison" int,
   "id_recette_parente" int,
   "url_photo" varchar
+);
+
+CREATE TABLE "tag_recette" (
+  "id_tag" int,
+  "id_recette" int,
+  PRIMARY KEY ("id_tag", "id_recette")
 );
 
 CREATE TABLE "tag" (
@@ -69,8 +105,8 @@ CREATE TABLE "ustensile" (
 CREATE TABLE "ingredient" (
   "id" serial PRIMARY KEY,
   "nom" varchar UNIQUE,
-  "debut_saison" date,
-  "fin_saison" date,
+  "debut_saison" int,
+  "fin_saison" int,
   "url_photo" varchar
 );
 
@@ -101,6 +137,24 @@ CREATE TABLE "ingredient_recette" (
   PRIMARY KEY ("id_recette", "id_ingredient")
 );
 
+CREATE TABLE "allergene_ingredient" (
+  "id_ingredient" int,
+  "id_allergene" int,
+  PRIMARY KEY ("id_ingredient", "id_allergene")
+);
+
+CREATE TABLE "ustensile_recette" (
+  "id_recette" int,
+  "id_ustensile" int,
+  PRIMARY KEY ("id_recette", "id_ustensile")
+);
+
+CREATE TABLE "allergie_utilisateur" (
+  "id_allergene" int,
+  "id_utilisateur" uuid,
+  PRIMARY KEY ("id_allergene", "id_utilisateur")
+);
+
 CREATE TABLE "ingredient_exclu" (
   "id_ingredient" int,
   "id_utilisateur" uuid,
@@ -127,31 +181,17 @@ ALTER TABLE "ingredient_panier" ADD FOREIGN KEY ("id_panier") REFERENCES "panier
 
 ALTER TABLE "ingredient_panier" ADD FOREIGN KEY ("id_ingredient") REFERENCES "ingredient" ("id");
 
-CREATE TABLE "panier_utilisateur" (
-  "panier_id" serial,
-  "utilisateur_id" uuid,
-  PRIMARY KEY ("panier_id", "utilisateur_id")
-);
+ALTER TABLE "panier_utilisateur" ADD FOREIGN KEY ("id_utilisateur") REFERENCES "utilisateur" ("id");
 
-ALTER TABLE "panier_utilisateur" ADD FOREIGN KEY ("panier_id") REFERENCES "panier" ("id");
-
-ALTER TABLE "panier_utilisateur" ADD FOREIGN KEY ("utilisateur_id") REFERENCES "utilisateur" ("id");
-
+ALTER TABLE "panier_utilisateur" ADD FOREIGN KEY ("id_panier") REFERENCES "panier" ("id");
 
 ALTER TABLE "recette" ADD FOREIGN KEY ("id_createur") REFERENCES "utilisateur" ("id");
 
 ALTER TABLE "recette" ADD FOREIGN KEY ("id_recette_parente") REFERENCES "recette" ("id");
 
-CREATE TABLE "tag_recette" (
-  "tag_id" serial,
-  "recette_id" serial,
-  PRIMARY KEY ("tag_id", "recette_id")
-);
+ALTER TABLE "tag_recette" ADD FOREIGN KEY ("id_tag") REFERENCES "tag" ("id");
 
-ALTER TABLE "tag_recette" ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("id");
-
-ALTER TABLE "tag_recette" ADD FOREIGN KEY ("recette_id") REFERENCES "recette" ("id");
-
+ALTER TABLE "tag_recette" ADD FOREIGN KEY ("id_recette") REFERENCES "recette" ("id");
 
 ALTER TABLE "commentaire" ADD FOREIGN KEY ("id_utilisateur") REFERENCES "utilisateur" ("id");
 
@@ -173,38 +213,17 @@ ALTER TABLE "ingredient_recette" ADD FOREIGN KEY ("id_recette") REFERENCES "rece
 
 ALTER TABLE "ingredient_recette" ADD FOREIGN KEY ("id_ingredient") REFERENCES "ingredient" ("id");
 
-CREATE TABLE "ingredient_allergene" (
-  "ingredient_id" serial,
-  "allergene_id" serial,
-  PRIMARY KEY ("ingredient_id", "allergene_id")
-);
+ALTER TABLE "allergene_ingredient" ADD FOREIGN KEY ("id_ingredient") REFERENCES "ingredient" ("id");
 
-ALTER TABLE "ingredient_allergene" ADD FOREIGN KEY ("ingredient_id") REFERENCES "ingredient" ("id");
+ALTER TABLE "allergene_ingredient" ADD FOREIGN KEY ("id_allergene") REFERENCES "allergene" ("id");
 
-ALTER TABLE "ingredient_allergene" ADD FOREIGN KEY ("allergene_id") REFERENCES "allergene" ("id");
+ALTER TABLE "ustensile_recette" ADD FOREIGN KEY ("id_recette") REFERENCES "recette" ("id");
 
+ALTER TABLE "ustensile_recette" ADD FOREIGN KEY ("id_ustensile") REFERENCES "ustensile" ("id");
 
-CREATE TABLE "recette_ustensile" (
-  "recette_id" serial,
-  "ustensile_id" serial,
-  PRIMARY KEY ("recette_id", "ustensile_id")
-);
+ALTER TABLE "allergie_utilisateur" ADD FOREIGN KEY ("id_allergene") REFERENCES "allergene" ("id");
 
-ALTER TABLE "recette_ustensile" ADD FOREIGN KEY ("recette_id") REFERENCES "recette" ("id");
-
-ALTER TABLE "recette_ustensile" ADD FOREIGN KEY ("ustensile_id") REFERENCES "ustensile" ("id");
-
-
-CREATE TABLE "allergene_utilisateur" (
-  "allergene_id" serial,
-  "utilisateur_id" uuid,
-  PRIMARY KEY ("allergene_id", "utilisateur_id")
-);
-
-ALTER TABLE "allergene_utilisateur" ADD FOREIGN KEY ("allergene_id") REFERENCES "allergene" ("id");
-
-ALTER TABLE "allergene_utilisateur" ADD FOREIGN KEY ("utilisateur_id") REFERENCES "utilisateur" ("id");
-
+ALTER TABLE "allergie_utilisateur" ADD FOREIGN KEY ("id_utilisateur") REFERENCES "utilisateur" ("id");
 
 ALTER TABLE "ingredient_exclu" ADD FOREIGN KEY ("id_ingredient") REFERENCES "ingredient" ("id");
 
