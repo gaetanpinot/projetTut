2396f3ef52509076e45ee6034ef063a545fe0b64
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2024-12-19 00:35:05 +0200

    merge

---
+++ api/src/application/action/CreateRecettesAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace amap\application\action;
+use amap\application\renderer\JsonRenderer;
+use amap\infrastructure\service\interfaces\RecetteServiceInterface;
+use DI\Container;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CreateRecettesAction extends AbstractAction
+{
+    private RecetteServiceInterface $recetteService;
+
+    public function __construct(ContainerInterface $cont)
+    {
+        parent::__construct($cont);
+        $this->recetteService = $cont->get(RecetteServiceInterface::class);
+    }
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+        $data = $rq->getParsedBody();
+
+        if (empty($data['nom']) || empty($data['temps_preparation']) || empty($data['id_createur'])) {
+            return JsonRenderer::render($rs, 400, ['error' => 'Invalid input data']);
+        }
+
+        try {
+            $result = $this->recetteService->createRecette($data);
+            return JsonRenderer::render($rs, 201, $result);
+        } catch (\Exception $e) {
+            return JsonRenderer::render($rs, 500, ['error' => 'Internal server error', 'message' => $e->getMessage()]);
+        }
+    }
+}
