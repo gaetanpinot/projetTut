c026eb23f166a43c2849bfd8ebe869a2d3a87f9d
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-12-19 12:51:39 +0100

    data validation dans la connexion

--- api/src/application/action/ConnexionAction.php
+++ api/src/application/action/ConnexionAction.php
@@ -2,12 +2,15 @@
 
 namespace amap\application\action;
 
+use Opis\JsonSchema\Validator;
+use Opis\JsonSchema\Errors\ErrorFormatter;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 use Slim\Exception\HttpBadRequestException;
 use amap\application\action\AbstractAction;
 use amap\application\renderer\JsonRenderer;
+use amap\application\renderer\ValidationErrorRenderer;
 use amap\core\dto\CredentialsDTO;
 use amap\providers\auth\AuthInvalidException;
 use amap\providers\auth\AuthnProviderInterface;
@@ -15,14 +18,40 @@ use amap\providers\auth\AuthnProviderInterface;
 class ConnexionAction extends AbstractAction
 {
     private AuthnProviderInterface $authnProvider;
-    public function __construct(AuthnProviderInterface $authnProvider, LoggerInterface $logger)
+    private Validator $validator;
+    public function __construct(AuthnProviderInterface $authnProvider, LoggerInterface $logger, Validator $validator)
     {
+        $this->validator = $validator;
         $this->authnProvider = $authnProvider;
         parent::__construct($logger);
     }
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
+        $schema = (object) [
+            'type' => 'object',
+            'properties' => (object)[
+                'nom_utilisateur' => (object)[
+                    'type' => 'string',
+                    'minLength' => 4,
+                    'maxLength' => 100,
+                ],
+                'mot_de_passe' => (object)[
+                    'type' => 'string',
+                    'minLength' => 4,
+                    'maxLength' => 100,
+                ]
+            ],
+            'required' => ['nom_utilisateur','mot_de_passe']
+        ];
+
+        $data = (object) $rq->getParsedBody();
+
+        $resultValidation = $this->validator->validate($data, $schema);
+        $checkValidite = ValidationErrorRenderer::render($rs, $resultValidation);
+        if ($checkValidite != false) {
+            return $checkValidite;
+        }
 
         $body = $rq->getParsedBody();
         $credentials = new CredentialsDTO('', $body['mot_de_passe'], $body['nom_utilisateur']);
