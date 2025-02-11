49dbb05306485251cbd49ef56cf5e4de6bb8cf85
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2025-02-11 14:29:37 +0200

    merge fix

--- Front/src/app/Components/Main/connexion-inscrip/connexion/connexion.component.ts
+++ Front/src/app/Components/Main/connexion-inscrip/connexion/connexion.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ConnexionService } from '../../../../Services/connexion.service';
 import { LogInRequestBody } from '../../../../Interfaces/connexion-user.interface';
-<<<<<<< HEAD
 import { AuthStoreService } from '../../../../Services/store/AuthStore.service';
-=======
-import { Store } from '@ngxs/store';
->>>>>>> e6e7a859d056183fba2b9de051b4f6b8f474a9b1
 
 @Component({
   selector: 'app-connexion',
@@ -22,22 +18,13 @@ export class ConnexionComponent implements OnInit {
     mot_de_passe: new FormControl('', [Validators.required])
   })
 
-<<<<<<< HEAD
   constructor(private connectServ: ConnexionService, private authStore: AuthStoreService){
-=======
-  constructor(private connectServ: ConnexionService, private store: Store){
->>>>>>> e6e7a859d056183fba2b9de051b4f6b8f474a9b1
   }
 
   ngOnInit(): void {
 
   }
-<<<<<<< HEAD
   
-=======
-
-
->>>>>>> e6e7a859d056183fba2b9de051b4f6b8f474a9b1
   onSubmit() {
     console.log("ok")
     const body: LogInRequestBody = {
@@ -52,17 +39,11 @@ export class ConnexionComponent implements OnInit {
         next: (data) => {
           console.log(data);
 
-<<<<<<< HEAD
           this.authStore.setToken(data.token);
           console.log(this.authStore);
           
           //token
           console.log(this.authStore.getToken());
-=======
-          localStorage.setItem("token", data.token);
-          this.store.dispatch({token: data.token}).subscribe(() => {});
-
->>>>>>> e6e7a859d056183fba2b9de051b4f6b8f474a9b1
         },
         error: (err) => {
           console.error(err);
