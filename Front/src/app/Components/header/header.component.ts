cd373e2e4c1cd6b432fbcc21527e2c460deaedba
Sasha daza <sashadaza0902@gmail.com>
2025-02-25 09:06:21 +0100

    t

--- Front/src/app/Components/header/header.component.ts
+++ Front/src/app/Components/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import { isAuth, isClient } from '../../shared/functions/isAuth';
 import { switchTheme } from '../../shared/functions/switchTheme';
-
+import { AuthStoreService } from '../../Services/store/AuthStore.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'header-component',
@@ -15,15 +15,13 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
 
   }
+  token$: Observable<string | null>;
 
-  isAuthenticated(): boolean{
-    return isAuth();
+  constructor(protected authStore: AuthStoreService) {
+    this.token$ = this.authStore.token$; // Assurez-vous que `token$` est un observable
   }
 
   switchTheme(event: any){
     switchTheme();
-  }
-  isClientAuth(): boolean{
-    return isClient();
   }
 }
