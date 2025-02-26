1156908f93fcaa14d60433012f337dd646e74718
perot13u <mathis.perot8@etu.univ-lorraine.fr>
2025-02-26 15:47:09 +0100

    rdm ingre

--- Front/src/app/Components/Main/home/home.component.ts
+++ Front/src/app/Components/Main/home/home.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {RecetteService} from '../../../Services/recette.service';
 import {Recette} from '../../../Interfaces/recette.interface';
+import {Ingredient} from '../../../Interfaces/ingredient.interface';
+import {IngredientsServicesService} from '../../../Services/ingredients.services.service';
 
 @Component({
   selector: 'app-test-home',
@@ -12,18 +14,32 @@ import {Recette} from '../../../Interfaces/recette.interface';
 export class HomeComponent implements OnInit {
 
   recettes: Recette[] = [];
+  ingredients: Ingredient[] = [];
 
-  constructor(private recetteService: RecetteService)
+  constructor(private recetteService: RecetteService, private ingredientsService: IngredientsServicesService)
   {}
 
   ngOnInit() {
     this.loadRandomRecettes();
+    this.loadRandomIngredients();
   }
 
   loadRandomRecettes(): void {
     this.recetteService.getRandomRecettes().subscribe({
       next: (data) => {
         this.recettes = data;
+        console.log(data)
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  loadRandomIngredients(): void {
+    this.ingredientsService.getRandomIngredients().subscribe({
+      next: (data) => {
+        this.ingredients = data;
         console.log(data)
       },
       error: (err) => {
