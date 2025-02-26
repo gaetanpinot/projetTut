50ec6b310e660f6ce609653cda8edfa0db74323a
perot13u <mathis.perot8@etu.univ-lorraine.fr>
2025-02-26 15:39:24 +0100

    rdm ingre

--- Front/src/app/Components/Main/home/home.component.ts
+++ Front/src/app/Components/Main/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecetteService} from '../../../Services/recette.service';
+import {Recette} from '../../../Interfaces/recette.interface';
 
 @Component({
   selector: 'app-test-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  recettes: Recette[] = [];
+
+  constructor(private recetteService: RecetteService)
+  {}
+
+  ngOnInit() {
+    this.loadRandomRecettes();
+  }
+
+  loadRandomRecettes(): void {
+    this.recetteService.getRandomRecettes().subscribe({
+      next: (data) => {
+        this.recettes = data;
+        console.log(data)
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
 }
