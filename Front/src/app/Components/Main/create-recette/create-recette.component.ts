949da4573c529ee05a373dac27c0dccc6280b391
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-02-27 16:40:54 +0100

    createion recette

--- Front/src/app/Components/Main/create-recette/create-recette.component.ts
+++ Front/src/app/Components/Main/create-recette/create-recette.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IngredientsServicesService } from '../../../Services/ingredients.services.service';
 import { RecetteService } from '../../../Services/recette.service';
 import { FormBuilder } from '@angular/forms';
+import { Ingredient } from '../../../Interfaces/ingredient.interface';
 
 @Component({
   selector: 'app-create-recette',
@@ -11,10 +12,26 @@ import { FormBuilder } from '@angular/forms';
   styleUrl: './create-recette.component.scss'
 })
 export class CreateRecetteComponent {
+
   constructor(
     private ingredientService: IngredientsServicesService,
     private recetteService: RecetteService,
     private fb: FormBuilder,
   ) {
   }
+  ingredients: Ingredient[] = [];
+
+  loadIngredients() {
+    this.ingredientService.getIngredients().subscribe(
+      {
+        next: (data) => {
+          this.ingredients = data;
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      }
+    );
+  }
+
 }
